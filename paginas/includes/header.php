4ac23dfd440625e9e3ca274e707304db4cd2c677
joyce-fcosta <joyced.fcosta@gmail.com>
2020-10-28 16:14:36 -0300

    primeiro commit

---
+++ paginas/includes/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="public/css/bootstrap/cssbootstrap.min.css">
+
+    <!-- Meu CSS -->
+    <link rel="stylesheet" href="public/css/meuCSS.css">
+
+
+    <title>Portifólio</title>
+</head>
+<body>
+    
+    <!-- jQuery! -->
+    <link rel="stylesheet" href="public/js/jquery-3.5.1.min.js">
+
+    <!-- Meu Script -->
+    <script src="public/js/meu_Script.js" ></script>
+</body>
+</html>
