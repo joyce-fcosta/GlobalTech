7cc5946dbcd20af35ac233addc63fb479f0b3d23
joyce-fcosta <joyced.fcosta@gmail.com>
2020-10-28 17:02:08 -0300

    commit atualizado

--- paginas/includes/header.php
+++ paginas/includes/header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Bootstrap -->
-    <link rel="stylesheet" href="public/css/Bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="public/css/bootstrap/css/bootstrap.min.css">
 
     <!-- Meu CSS -->
     <link rel="stylesheet" href="public/css/meuCSS.css">
